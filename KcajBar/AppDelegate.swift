f730f81e1902f698bfb23e9bad2d6759a06738ae
Noskcaj <kcajdev@gmail.com>
2018-02-21 17:10:40 -0500

    Initial Commit

---
+++ KcajBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  KcajBar
+//
+//  Created by Jack Nunley on 2/21/18.
+//  Copyright © 2018 Noskcaj. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
